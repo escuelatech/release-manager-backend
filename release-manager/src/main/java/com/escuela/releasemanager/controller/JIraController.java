b3e12a6cb66efc35ae43332b2ab961700f758aaa
Hasitha Maduranga <Hasitha.Ma@creativesoftware.com>
2021-04-02 01:28:44 +0530

    filter project and lable

--- release-manager/src/main/java/com/escuela/releasemanager/controller/JIraController.java
+++ release-manager/src/main/java/com/escuela/releasemanager/controller/JIraController.java
@@ -1,9 +1,8 @@
 package com.escuela.releasemanager.controller;
 
-import com.escuela.releasemanager.model.IssueModel;
+import com.escuela.releasemanager.api.JiraService;
 import com.escuela.releasemanager.model.IssueSearchModel;
 import com.escuela.releasemanager.model.ProjectModel;
-import com.escuela.releasemanager.api.JiraService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 
 @RestController
@@ -25,15 +22,15 @@ public class JIraController {
     JiraService jiraService;
 
     @GetMapping("/getall")
-    public ResponseEntity<List<ProjectModel>> getProjectDetails(){
+    public ResponseEntity<List<ProjectModel>> getProjectDetails() {
         return ResponseEntity.ok(jiraService.getAllJiraProjects());
 
     }
 
     @GetMapping("/getall/issues")
-    public ResponseEntity<IssueSearchModel> getProjectIssuesByLabel(@RequestParam(name = "labels") String labels){
+    public ResponseEntity<IssueSearchModel> getProjectIssuesByLabel(@RequestParam(name = "project") String project, @RequestParam(name = "labels") String labels) {
 
-        return ResponseEntity.ok(jiraService.getProjectIssuesByLabel(labels));
+        return ResponseEntity.ok(jiraService.getProjectIssuesByLabel(project, labels));
 
     }
 }
