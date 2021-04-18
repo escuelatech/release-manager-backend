ddf04695a46bca90cad6677ef3a2af14e90e7717
Escuela Technologies <escuela.tech@gmail.com>
2021-04-18 11:23:14 -0500

    Registration controller

---
+++ release-manager/src/main/java/com/escuela/releasemanager/controller/ProjectRegistrationConroller.java
@@ -0,0 +1,10 @@
+package com.escuela.releasemanager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/mapping")
+public class ProjectRegistrationConroller {
+
+}
